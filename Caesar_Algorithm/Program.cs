f1a487c2857c2e7d48bfc026aec06ce4487e0667
HMK <hasanmusa05g@hotmail.com>
2023-02-22 16:03:19 +0300

    linear and caesar crypt algorithms added.

---
+++ Caesar_Algorithm/Program.cs
@@ -0,0 +1,65 @@
+﻿
+string chr = "abcçdefgğhıijklmnoöprsştuüvyz";
+
+getinput:
+Console.Write("\nGirdi giriniz: ");
+string input = Console.ReadLine().ToLower().Trim();
+Console.WriteLine("\nŞifreleme(1)\nŞifre Çözme(2)");
+short process = Convert.ToInt16(Console.ReadLine());
+
+if (process == 1)
+{
+    if (input.Length == 0)
+        goto getinput;
+
+    List<char> newstring = new List<char>();
+    for (int i = 0; i < input.Length; i++)
+    {
+        foreach (var chars in chr)
+        {
+            if (input[i] == chars)
+            {
+                var crypted = chr[chr.IndexOf(chars) + 3];
+                newstring.Add(crypted);
+            }
+        }
+    }
+    Console.Write("\nGirdinin Sezar şirelenmiş hâli: ");
+    foreach (var item in newstring)
+    {
+        Console.Write(item);
+    }
+    goto getinput;
+}
+
+
+if (process == 2)
+{
+    if (input.Length == 0)
+        goto getinput;
+
+    List<char> newstring = new List<char>();
+    for (int i = 0; i < input.Length; i++)
+    {
+        foreach (var chars in chr)
+        {
+            if (input[i] == chars)
+            {
+                var index = chr.IndexOf(chars) - 3;
+                if (index <= 0)
+                {
+                    index = chr.Length + chr.IndexOf(chars)-3;
+                }
+                var crypted = chr[index];
+                newstring.Add(crypted);
+            }
+        }
+    }
+    Console.Write("\nGirdinin çözülmüş hâli: ");
+    foreach (var item in newstring)
+    {
+        Console.Write(item);
+    }
+    goto getinput;
+}
+
---
+++ Linear_Crypt_Algorithm/Program.cs
@@ -0,0 +1,81 @@
+﻿
+
+string chr = "abcçdefgğhıijklmnoöprsştuüvyz";
+
+getinput:
+Console.Write("\nDoğrusal şifreleme için a anahtarını giriniz:");
+short inputa = Convert.ToInt16(Console.ReadLine());
+Console.Write("\nDoğrusal şifreleme için b anahtarını giriniz:");
+short inputb = Convert.ToInt16(Console.ReadLine());
+Console.Write("\nDoğrusal şifreleme için girdi giriniz:");
+var text = Console.ReadLine().ToLower().Trim();
+Console.WriteLine("\nŞifreleme(1)\nŞifre Çözme(2)");
+short process = Convert.ToInt16(Console.ReadLine());
+List<char> result = new List<char>();
+
+if (process == 1)
+{
+    for (int i = 0; i < text.Length; i++)
+    {
+        foreach (var item in chr)
+        {
+            if (text[i] == item)
+            {
+                var index = (inputa * chr.IndexOf(item) + inputb) % chr.Length;
+                result.Add(chr[index]);
+
+            }
+        }
+    }
+
+    Console.Write("\nGirdinin şifrelenmiş hâli: ");
+    foreach (var item in result)
+    {
+        Console.Write(item);
+    }
+    goto getinput;
+}
+
+if (process == 2)
+{
+    int mters = 1;
+    for (int modters = 1; modters < chr.Length; modters++)
+    {
+        if ((inputa * modters) % chr.Length == 1)
+        {
+            mters = modters;
+            break;
+        }
+    }
+    for (int i = 0; i < text.Length; i++)
+    {
+        foreach (var chars in chr)
+        {
+            if (text[i] == chars)
+            {
+                var charindex = chr.IndexOf(chars);
+
+                if (charindex - inputb < 0)
+                {
+                    charindex = charindex + chr.Length;
+                }
+
+                var index = mters * (charindex - inputb) % chr.Length;
+                result.Add(chr[index]);
+
+            }
+
+        }
+    }
+
+    Console.Write("\nGirdinin çözülmüş hâli: ");
+    foreach (var item in result)
+    {
+        Console.Write(item);
+    }
+    goto getinput;
+}
+
+
+
+
